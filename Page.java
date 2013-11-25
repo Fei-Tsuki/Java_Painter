31843bea7d2249811d581011a51f47dc5396f844
Fei-Tsuki <jim820419@gmail.com.tw>
2013-11-25 08:51:28 +0800

    Change the Page of the Comment

--- Page.java
+++ Page.java
@@ -1,21 +1,20 @@
-
 package painter;
 import java.awt.*;
 import javax.swing.*;
 
-public class Page extends JPanel{
+public class Page extends JPanel{   
     Page()
     {
-        this.setBackground(Color.yellow);
-        this.setLayout(null);
+        this.setBackground(Color.yellow);   // Set the background of the color is yellow
+        this.setLayout(null);               // Painter_Page don't use the Layout
     }
     
     public void paintComponent(Graphics pen)
     {
-        super.paintComponent(pen);
-        pen.setPaintMode();
-        pen.setColor(Color.black);
-        pen.drawLine(0, 0, 200, 200);
+        super.paintComponent(pen);          // Get the pen from System
+        pen.setPaintMode();                 // Set the Painter Mode
+        pen.setColor(Color.black);          // The pen's color is black
+        pen.drawLine(0, 0, 200, 200);       // draw a line (0,0) ~ (200,200)
     }
     
     
